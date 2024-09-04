19ad7c02dc88812ce7d21843debe8de9a1928249
hana <wrtui@naver.com>
2024-09-04 12:47:47 +0900

    prod.py 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -12,10 +12,10 @@ environ.Env.read_env(BASE_DIR / '.env')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD':'Y,f#D5Wfp&m*R8gb(,EMAc[SzWM6+j2D' ,
+        'HOST': 'ls-b526c35546c4c984afa0256466afad5d7698ceda.cbg28k60wken.ap-northeast-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
